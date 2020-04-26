13b8b9dacb55ec72ca6ec76250595fb4ece28e08
Татьяна <kornieva.tatiana@gmail.com>
2020-04-26 08:55:49 +0300

    Create the New Project

---
+++ src/test/java/com/example/SimpleTest.java
@@ -0,0 +1,15 @@
+package com.example;
+
+
+import org.testng.annotations.Test;
+
+public class SimpleTest {
+
+    @Test
+    public void calculationTest () {
+        System.out.println("simple test");
+
+    }
+
+}
+
